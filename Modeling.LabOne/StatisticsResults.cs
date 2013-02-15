65353e18e6a8915d5499e31879b5e34417c7b2a0
Roman Ilyenko <ilienko.roman@gmail.com>
2013-02-16 01:06:36 +0300

    Initial commit
    up

---
+++ Modeling.LabOne/StatisticsResults.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modeling.LabOne
+{
+    public class StatisticsResults
+    {
+        public Double ExpectedValue { get; set; }
+
+        public Double Variance { get; set; }
+
+        public Double Deviation { get; set; }
+
+        
+    }
+}
